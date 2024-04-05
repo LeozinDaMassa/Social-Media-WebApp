81efc861198d1e2562b9af68d6b32e65bc72f7b0
Leonardo Storti Martins <leonardo.storti@outlook.com>
2024-04-05 02:47:00 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
-import axios from 'axios';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-      </div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Navigate to="/login" />
+      </Routes>
     </Router>
   );
 }
-
-const API_URL = 'http://localhost:5000/api/auth';
-
-axios.post(`${API_URL}/register`)
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
 
 export default App;
